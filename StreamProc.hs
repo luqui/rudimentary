aac51c0b6929e89daaad3968d8f44cebcf22fe06
Luke Palmer <lrpalmer@gmail.com>
2016-09-01 20:50:18 -0700

    A lot of useful functions on StreamProcs

--- StreamProc.hs
+++ StreamProc.hs
@@ -1,9 +1,13 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving, LambdaCase, BangPatterns #-}
 
 import Control.Monad (liftM, ap, (>=>))
+import Control.Arrow (first)
 
 newtype TimeDiff = TimeDiff Double
     deriving (Eq, Ord, Num)
+
+infinity :: TimeDiff
+infinity = TimeDiff (1/0)
 
 data Future i a 
     = Wait TimeDiff (Maybe (TimeDiff, i) -> Future i a)
@@ -17,18 +21,80 @@ instance Monad (Future i) where
 instance Functor (Future i) where fmap = liftM
 instance Applicative (Future i) where pure = return; (<*>) = ap
 
+mapIF :: (i' -> i) -> Future i a -> Future i' a
+mapIF f (Wait d f') = Wait d (mapIF f . f' . (fmap.fmap) f)
 
-data Train f o a 
-    = Car (f (o, Train f o a))
+waitF :: TimeDiff -> Future i (Maybe i)
+waitF d = Wait d (return . fmap snd)
+
+waitTimeF :: TimeDiff -> Future i ()
+waitTimeF d = Wait d $ \case
+    Nothing -> return ()
+    Just (d',_) -> waitTimeF (d-d')
+
+waitForeverF :: Future i i
+waitForeverF = Wait infinity (\(Just (_, x)) -> return x)
+
+tagTimeF :: Future i a -> Future i (TimeDiff, a)
+tagTimeF (Return x) = Return (0, x)
+tagTimeF (Wait d f) = Wait d $ \case
+    Nothing -> fmap (first (+d)) (tagTimeF (f Nothing))
+    i@(Just (d',_)) -> fmap (first (+d')) (tagTimeF (f i))
+
+
+
+data StreamProc i o a 
+    = Input (Future i (StreamProc i o a))
+    | Output o (StreamProc i o a)
     | Caboose a
 
-instance (Functor f) => Monad (Train f o) where
+instance Monad (StreamProc i o) where
     return = Caboose
+    Input f >>= t = Input (fmap (>>= t) f)
+    Output o s >>= t = Output o (s >>= t)
     Caboose a >>= t = t a
-    Car f >>= t = Car ((fmap.fmap) (>>= t) f)
 
-instance (Functor f) => Functor (Train f o) where fmap = liftM
-instance (Functor f) => Applicative (Train f o) where pure = return; (<*>) = ap
+instance Functor (StreamProc i o) where fmap = liftM
+instance Applicative (StreamProc i o) where pure = return; (<*>) = ap
+
+mapI :: (i' -> i) -> StreamProc i o a -> StreamProc i' o a
+mapI f (Input c) = Input (fmap (mapI f) (mapIF f c))
+mapI f (Output o p) = Output o (mapI f p)
+mapI _ (Caboose a) = Caboose a
+
+mapO :: (o -> o') -> StreamProc i o a -> StreamProc i o' a
+mapO f (Input c) = Input (fmap (mapO f) c)
+mapO f (Output o p) = Output (f o) (mapO f p)
+mapO _ (Caboose a) = Caboose a
 
 
-type StreamProc i o a = Train (Future i) o a
+fromFuture :: Future i a -> StreamProc i o a
+fromFuture = Input . fmap return
+
+wait :: TimeDiff -> StreamProc i o (Maybe i)
+wait = fromFuture . waitF
+
+waitTime :: TimeDiff -> StreamProc i o ()
+waitTime = fromFuture . waitTimeF
+
+waitForever :: StreamProc i o i
+waitForever = fromFuture waitForeverF
+
+output :: o -> StreamProc i o ()
+output x = Output x (Caboose ())
+
+identity :: StreamProc i i a
+identity = waitForever >>= \x -> output x >> identity
+
+tagTimeHelper :: StreamProc i o a -> StreamProc i (TimeDiff, o) (TimeDiff, a)
+tagTimeHelper = go 0
+    where   
+    go !accum (Input f) = Input (fmap (\(t,p) -> go (accum+t) p) (tagTimeF f))
+    go !accum (Output o p) = Output (accum,o) (go accum p)
+    go !accum (Caboose a) = Caboose (accum,a)
+
+tagTime :: StreamProc i o a -> StreamProc i o (TimeDiff, a)
+tagTime = mapO snd . tagTimeHelper
+
+tagTimeOutputs :: StreamProc i o a -> StreamProc i (TimeDiff, o) a
+tagTimeOutputs = fmap snd . tagTimeHelper
