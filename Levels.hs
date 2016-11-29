a77d2ab74f0e0246bacdec17a007937bd7a5e17f
Luke Palmer <lrpalmer@gmail.com>
2016-11-29 01:12:28 -0800

    "Implemented" (untested) levels on the server side, and started new levels structure

--- Levels.hs
+++ Levels.hs
@@ -1,7 +1,8 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE TupleSections, GeneralizedNewtypeDeriving #-}
 
 module Levels where
 
+import Control.Applicative
 import Control.Monad (join)
 import qualified Control.Monad.Random as Rand
 
@@ -21,41 +22,19 @@ dist choices = do
 uniform :: [a] -> Dist a
 uniform = dist . map (1,)
 
-levels :: [(String, Dist Exp)]
-levels = [
-    "Natural Modes of C" --> Exp <$> (Scale <$> pure (Note 0) <*> naturalMode) <*> asc,
-    "Melodic Modes of C" --> Exp <$> (Scale <$> pure (Note 0) <*> melodicMode) <*> asc,
-    "Modes of C"         --> 
-        Exp <$> (Scale <$> pure (Note 0) <*> join (uniform [naturalMode, melodicMode])) <*> asc,
-
-    "Intervals from C" -->
-        Exp (Scale (Note 0) (Mode Natural 7)) <$> do {
-            n <- uniform [1..7];
-            acc <- uniform [-1..1]; 
-            return (DEParallel (DERun [Degree 0 0]) (DERun [Degree n acc])) },
+data Level = Level {
+    levelName :: String,
+    levelDesc :: String,
+    levelSpec :: Dist (Exp, String -> Bool) }
 
 
-    "Chords of C major" --> Exp (Scale (Note 0) (Mode Natural 7)) <$> chord 3,
-    "Chords of Natural Modes of C (3 notes)" -->  
-        Exp <$> (Scale <$> pure (Note 0) <*> naturalMode) <*> chord 3,
-    "Chords of Natural Modes of C (4 notes)" --> 
-        Exp <$> (Scale <$> pure (Note 0) <*> naturalMode) <*> chord 4
-    ]
-    where
-    infix 1 -->
-    (-->) = (,)
-
-    asc = pure (DERun (map (`Degree` 0) [0..7]))
-    naturalMode = Mode <$> pure Natural <*> uniform [1..7]
-    melodicMode = Mode <$> pure Melodic <*> uniform [1..7]
-
-    chord :: Int -> Dist DegreeExp
-    chord = fmap (foldr1 DEParallel . map (\n -> DERun [Degree n 0])) . go
-        where
-        go n = do
-            n1 <- uniform [0..6]
-            (n1:) <$> go' (n-1) n1
-        go' 0 _ = return []
-        go' n accum = do
-            ni <- uniform [1..3]
-            ((accum+ni) :) <$> go' (n-1) (accum+ni)
+level1 :: Level
+level1 = Level {
+    levelName = "Major scale intervals",
+    levelDesc = concat ["I will play the 1 and then another degree of the major scale, ",
+                        "and you need to identify which degree that is as a number."],
+    levelSpec = do
+        i <- uniform [1..7]
+        return (Exp (Scale (Note 0) (Mode Natural 1)) (DERun [Degree 0 0, Degree i 0]), 
+                (== show i))
+  }
--- server.hs
+++ server.hs
@@ -1,20 +1,32 @@
-{-# LANGUAGE OverloadedStrings, OverloadedLists #-}
+{-# LANGUAGE OverloadedStrings, OverloadedLists, MultiWayIf #-}
 
-import Data.Monoid ((<>))
+import Control.Concurrent.MVar
+import Control.Concurrent (forkIO)
 import Control.Monad.Trans (liftIO)
+import Data.Monoid ((<>))
 import Data.String (fromString)
 import GHC.Exts (fromList)
 
+import qualified Control.Monad.Random as Rand
 import qualified Data.Aeson as J
+import qualified Data.Map as Map
 import qualified System.MIDI as SysMid
 import qualified Web.Scotty as S
 
 import qualified MIDI
 import qualified Syntax
 import qualified Semantics
+import qualified Levels
+
+data Session = Session {
+    sessionLevel :: Levels.Level,
+    sessionAttempt :: Maybe (Syntax.Exp, String -> Bool) }
 
 main :: IO ()
-main = S.scotty 3000 $ do
+main = do
+  sessionCounter <- liftIO $ newMVar 0
+  sessions <- liftIO $ newMVar (Map.empty :: Map.Map Int Session)
+  S.scotty 3000 $ do
     S.get "/" $ do
         S.setHeader "Content-type" "text/html"
         S.file "index.html"
@@ -32,7 +44,7 @@ main = S.scotty 3000 $ do
                 "message" J..= J.String (fromString (show err)) ]
             Right expr -> do
                 dev <- liftIO $ MIDI.connectOutput devname
-                liftIO $ MIDI.playNotes 1 (Semantics.evalExp expr) dev
+                liftIO . forkIO $ MIDI.playNotes 1 (Semantics.evalExp expr) dev
                 S.json $ J.object [
                     "type" J..= J.String "success" ]
 
@@ -40,3 +52,43 @@ main = S.scotty 3000 $ do
         devnames <- liftIO $ mapM SysMid.getName =<< SysMid.enumerateDestinations
         S.json $ J.Array (J.String . fromString <$> fromList devnames)
 
+    S.get "/level" $ do
+        --levelname <- S.param "name"
+        level <- return Levels.level1
+        sessionid <- liftIO $ modifyMVar sessionCounter (\i -> return (i+1,i))
+        liftIO $ modifyMVar_ sessions (return . Map.insert sessionid (Session level Nothing))
+
+        S.json $ J.object [
+            "name" J..= J.String (fromString (Levels.levelName level)),
+            "desc" J..= J.String (fromString (Levels.levelDesc level)),
+            "session" J..= J.Number (fromIntegral sessionid) ]
+
+    S.get "/trylevel" $ do
+        devname <- S.param "dev"
+        sessionid <- S.param "session"
+        answer <- S.param "answer"
+
+        session <- liftIO $ (Map.! sessionid) <$> readMVar sessions
+        dev <- liftIO $ MIDI.connectOutput devname
+
+        (S.json =<<) . liftIO $ case sessionAttempt session of
+            Nothing -> do
+                (expr, ans) <- Rand.evalRandIO (Levels.levelSpec (sessionLevel session))
+                forkIO $ MIDI.playNotes 1 (Semantics.evalExp expr) dev
+                modifyMVar_ sessions $
+                    return . Map.insert sessionid (session { sessionAttempt = Just (expr, ans) })
+                return $ J.object []
+            Just (expr, ans) -> do
+                forkIO $ MIDI.playNotes 1 (Semantics.evalExp expr) dev
+                if | null answer -> return $ J.object []
+                   | ans answer -> do
+                        modifyMVar_ sessions $
+                            return . Map.insert sessionid (session { sessionAttempt = Nothing })
+                        return $ J.object [
+                            "correct" J..= J.Bool True,
+                            "notation" J..= Syntax.pretty expr ]
+                   | otherwise -> return $ J.object [
+                        "correct" J..= J.Bool False ]
+
+    S.get "/levels" $ do
+        S.json $ J.Array ["Level 1"]
