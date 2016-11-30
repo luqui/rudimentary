074f8a323ad330e5a62d99820d9579fc6d64eaeb
Luke Palmer <lrpalmer@gmail.com>
2016-11-29 19:18:38 -0800

    Added chord motion level.

--- Levels.hs
+++ Levels.hs
@@ -3,11 +3,14 @@
 module Levels where
 
 import Control.Applicative
+import Control.Arrow ((&&&))
 import Control.Monad.Trans.State
 import Control.Monad.Trans.Writer
-import Control.Monad (join)
+import Control.Monad (join, replicateM)
 import qualified Data.Map as Map
 import qualified Control.Monad.Random as Rand
+import qualified Text.Parsec as Parsec
+import qualified Text.Parsec.Token as Parsec
 
 import Syntax
 import qualified Params as P
@@ -44,7 +47,8 @@ chooseLevels l = join . P.select l l . map toTuple
 levels :: Level
 levels = chooseLevels "Level" [
     intervals,
-    chordQualities ]
+    chordQualities,
+    chordMotion ]
 
 select :: String -> [(String, String, a)] -> Params a
 select title = P.select title title
@@ -168,7 +172,12 @@ chordQualities = Sublevel {
                     grade selectFrom chord)
     }
     where
-    chordTypes = Map.fromList [
+    grade selectFrom correct ans
+        | ans `elem` selectFrom = Just (correct == ans)
+        | otherwise = Nothing
+
+chordTypes :: Map.Map String [Degree]
+chordTypes = Map.fromList [
         ("maj", [Degree 0 0, Degree 2 0, Degree 4 0]),
         ("min", [Degree 0 0, Degree 2 (-1), Degree 4 0]),
         ("dim", [Degree 0 0, Degree 2 (-1), Degree 4 (-1)]),
@@ -181,10 +190,6 @@ chordQualities = Sublevel {
         ("aug7", [Degree 0 0, Degree 2 0, Degree 4 1, Degree 6 (-1)]),
         ("minmaj", [Degree 0 0, Degree 2 (-1), Degree 4 0, Degree 6 0]) ]
 
-    grade selectFrom correct ans
-        | ans `elem` selectFrom = Just (correct == ans)
-        | otherwise = Nothing
-
 
 selectVoicing :: Params ([Degree] -> Dist [Degree])
 selectVoicing = select "Voicing"
@@ -193,6 +198,9 @@ selectVoicing = select "Voicing"
        "ascending order.  This is the simplest voicing and should be used to get " ++
        "familiar with new chord qualities.",
        return)
+    , ("Closed with Bass",
+       "Different notes might be on top, but a bass note is added at the root.",
+       closedBassNote)
     , ("Closed",
        "All the notes of the chord are as close together as possible, but the root might " ++
        "not be on the bottom.",
@@ -203,8 +211,102 @@ selectVoicing = select "Voicing"
     where
     randomInversion chord = do
         invUp <- uniform [0..length chord-1]
-        return $ [ Degree (n+7) alt | Degree n alt <- take invUp chord ] ++ drop invUp chord
+        octShift <- uniform [0,-7]
+        return $ [ Degree (octShift+n+7) alt | Degree n alt <- take invUp chord ] ++ 
+                 [ Degree (octShift+n) alt | Degree n alt <- drop invUp chord ]
+
+    closedBassNote chord@(Degree n alt:_) = do
+        inv <- randomInversion chord
+        octmod <- uniform [-7,-14]
+        return $ Degree (n+octmod) alt : inv
 
     openVoicing chord = do
         mapM (\(Degree n alt) -> Degree <$> ((n+) <$> uniform [-7,0,7]) <*> pure alt) chord
 
+
+chordMotion :: Sublevel
+chordMotion = Sublevel {
+    slName = "Chord Motion",
+    slDesc = "I'll play a series of chords starting on the I, and you tell me what they are " ++
+             "in terms of the I.  For example <code>I iii V ivo</code> is a major 1 chord, " ++
+             "a minor 3 chord, a major 5 chord, and a diminished 7 chord (<code>o</code> means "++
+             "diminished.",
+    slLevel = do
+        sequenceLength <- select' "Sequence Length"
+            "How many chords am I going to give you?"
+            [ ("2", 2), ("3", 3), ("4", 4), ("5", 5) ]
+        
+        voicing <- selectVoicing
+        keyDist <- selectKey
+
+        modeDist <- select "Mode"
+            [ ("Major", "<code>I ii iii IV V vi viio</code>", return (Mode Natural 7))
+            , ("Minor", "<code>i iio III iv v VI VII</code>", return (Mode Natural 5))
+            , ("Dorian", "<code>i ii III IV v vio VII</code>", return (Mode Natural 1))
+            , ("Common Modes", "Major, minor, and dorian (also harmonic minor but not implemented)",
+               uniform [Mode Natural 7, Mode Natural 5, Mode Natural 1])
+            , ("Natural Modes",
+               "All natural modes (memorizing these would be silly, just listen for the qualities.",
+               uniform [Mode Natural n | n <- [1..7]]) ]
+
+        includeSevens <- select "Include 7s"
+            [ ("No", "Just triads will be used.", False)
+            , ("Yes", "Tetrachords will be used, but don't worry about transcribing more than "++
+                      "their base qualities (minor, major, or diminished)", True)
+            ]
+
+        return $ do
+            degrees <- (0:) <$> replicateM (sequenceLength-1) (uniform [0..6])
+            let notess = [ [ Degree n 0 | n <- [i,i+2,i+4] ++ [ i+6 | includeSevens ]] 
+                         | i <- degrees ]
+            notess' <- mapM voicing notess
+            mode <- modeDist
+            key <- keyDist
+
+            return (Exp (Scale key mode)
+                        (foldr1 DEConcat (map (foldr1 DEParallel . map (DERun. (:[]))) notess')),
+                    grade mode degrees)
+    }
+    where
+    identifyChord mode deg = 
+        case diffSeq $ map (render (Scale (Note 0) mode) . flip Degree 0) (map (deg+) [0,2,4]) of
+            [4,3] -> QMajor
+            [3,4] -> QMinor
+            [3,3] -> QDiminished
+            is -> error $ "Cannot identify chord with intervals " ++ show is
+        where
+        render scale = Semantics.applyScale (Semantics.renderScale scale)
+    diffSeq s = zipWith (-) (tail s) s
+
+    grade mode degrees s = case parseString parseDegrees s of
+        Left _ -> Nothing
+        Right ans -> let expected = map (id &&& identifyChord mode) degrees in
+                     Just $ ans == expected
+
+data BaseQuality = QMajor | QMinor | QDiminished
+    deriving (Eq, Show)
+
+parseDegrees :: Parser [(Int, BaseQuality)]
+parseDegrees = Parsec.many1 degree
+    where
+    majDegree = Parsec.choice (map Parsec.try [
+            2 <$ Parsec.symbol tok "III",
+            3 <$ Parsec.symbol tok "IV",
+            1 <$ Parsec.symbol tok "II",
+            0 <$ Parsec.symbol tok "I",
+            6 <$ Parsec.symbol tok "VII",
+            5 <$ Parsec.symbol tok "VI",
+            4 <$ Parsec.symbol tok "V"
+        ])
+    minDegree = Parsec.choice (map Parsec.try [
+            2 <$ Parsec.symbol tok "iii",
+            3 <$ Parsec.symbol tok "iv",
+            1 <$ Parsec.symbol tok "ii",
+            0 <$ Parsec.symbol tok "i",
+            6 <$ Parsec.symbol tok "vii",
+            5 <$ Parsec.symbol tok "vi",
+            4 <$ Parsec.symbol tok "v"
+        ])
+    degree :: Parser (Int, BaseQuality)
+    degree = ((, QMajor) <$> majDegree) 
+         <|> ((,) <$> minDegree <*> Parsec.option QMinor (QDiminished <$ Parsec.symbol tok "o"))
